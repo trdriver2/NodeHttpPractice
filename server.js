bd7629e6d09f43fc07eed3dc09fffd49b5ed20dd
trdriver2 <trdriver@talentpath.com>
2019-10-16 15:26:17 -0500

    added quotes

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+function randomQuote(){
+    let quotes = ["quote 1", "quote 2", "quote 3", "quote 4", "quote 5", 
+    "quote 6", "quote 7", "quote 8", "quote 9", "quote 10"]
+    return quotes[(Math.random()*9).toFixed(0)]
+}
+
 const http = require('http');
 
 const port = 3000
@@ -9,7 +15,7 @@ const server = http.createServer((req,res) => {
     else
         req.statusCode=404
 
-    req.on('data', (chunk)=>{res.end(chunk)})
+    req.on('data', ()=>{res.end(randomQuote())})
 
     res.setHeader('Content-Type', 'text/plain'); 
     res.write('Test!\n'); //https://nodejs.org/api/http.html#http_response_end_data_encoding_callback
